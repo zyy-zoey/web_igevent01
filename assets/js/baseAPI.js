362453bedf45350de32238135c16bbef965ec462
zyy-zoey <593388979@qq.com>
2022-05-19 15:17:52 +0800

    添加baseAPI注释

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,6 +2,10 @@
 // 会先调用 ajaxPrefiler 这个函数
 // 在这个函数中，可以拿到我们给ajax提供的配置对象
 // 请求拦截器
-$.ajaxPrefilter((option) => {
+// 语法 ： $.ajaxPrefilter( [ dataType ,] handler )
+//dataType : 一个或多个用空格隔开的数据类型所组成的字符串。
+//handler--用于预处理参数选项的回调函数。它有以下3个参数：
+// options：(Object对象)当前AJAX请求的所有参数选项。
+$.ajaxPrefiler((option) => {
   option.url = `http://www.liulongbin.top:3007` + option.url;
 });
